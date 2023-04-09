7a518dedc9dcf9922f71014c728dcd80bc0be5dc
Wm Lennon (he/him) <milehighhokie@users.noreply.github.com>
2023-04-09 13:37:27 -0600

    Almost finished code

--- read.php
+++ read.php
@@ -1,58 +1,139 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Delicious+Handrawn&display=swap" rel="stylesheet">
+
 	<title>Prayer Website</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+			background-color: #F5F5F5;
+		}
+		header {
+			font-family: 'Pacifico', cursive;
+			background-color: #10846f;
+			color: #ffffff;
+			padding: 20px;
+			font-size: 20px;
+			display: flex;
+			display: flex;
+  			align-items: center;
+		}
+
+		h1 {
+			margin: 0;
+		}
+
+		header a {
+			margin-left: 50px;
+			font-family: 'Delicious Handrawn', cursive;
+			color: #ffffff;
+			font-size: 30px;
+		}
+
+		header a:hover {
+			color:#09344c;
+		}
+
+		main {
+			margin: 50px auto;
+			max-width: 800px;
+			padding: 20px;
+			background-color: #fff;
+			border: 1px solid #ccc;
+		}
+		table {
+			width: 100%;
+			border-collapse: collapse;
+		}
+		th {
+			background-color: #5a8057;
+			color: #fff;
+			padding: 10px;
+			text-align: left;
+			border: 1px solid #ccc;
+		}
+		td {
+			border: 1px solid #ccc;
+			padding: 10px;
+			text-align: left;
+		}
+		td a {
+			color: #333;
+			text-decoration: none;
+			word-wrap: break-word;
+		}
+		td a:hover {
+			text-decoration: underline;
+		}
+		footer {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+			position: fixed;
+			bottom: 0;
+			left: 0;
+			right: 0;
+		}
+	</style>
 </head>
 <body>
 	<header>
 		<h1>Prayer Website</h1>
+		<a href="index.html">Home</a>
+		<a href="write.html">Write Prayer</a>
 	</header>
 	<main>
-		<p>READ PAGE</p>
-	</main>
-	
-	
-
 		<?php
-		$link = mysqli_connect("localhost", "milehigh_prayers", "1234userPRAYERS", "milehigh_prayers"); 
-		  
-		if ($link == false) { 
-			die("ERROR: Could not connect. "
-						.mysqli_connect_error()); 
-		} 
+			$link = mysqli_connect("localhost", "milehigh_prayers", "1234userPRAYERS", "milehigh_prayers"); 
+			if ($link == false) { 
+				die("ERROR: Could not connect. " . mysqli_connect_error()); 
+			}
+			
+			$sql = "SELECT p.* FROM prayers p WHERE 1=1"; 
 
-		$sql = "
-		SELECT p.* 
-		FROM prayers p
-		WHERE 1=1
-		"; 
-
-		if ($res = mysqli_query($link, $sql)) { 
-			if (mysqli_num_rows($res) > 0) { 
-				echo "<table bgcolor=white valign=top>"; 
-				while ($row = mysqli_fetch_array($res)) { 
-					echo "<tr><td>";
-					echo "<a idprayers=".$row['idprayers'].">";
-					echo $row['prayerText'].",".$row['prayerEntryDate']." (".$row['prayerReadCount'].")";
-					echo "</a></td></tr>"; 
+			if ($res = mysqli_query($link, $sql)) { 
+				if (mysqli_num_rows($res) > 0) { 
+					echo "<table>"; 
+					echo "<tr>";
+					echo "<th>Prayer</th>";
+					echo "<th>Entry Date</th>";
+					echo "<th>Number of times read</th>";
+					echo "</tr>";
+					while ($row = mysqli_fetch_array($res)) {
+					    $res2 = mysqli_query($link, "UPDATE prayers SET prayersReadCount = prayersReadCount + 1 WHERE idprayers=".$row['idprayers']);
+						echo "<tr>";
+						echo "<td>";
+						echo "<a idprayers=".$row['idprayers'].">";
+						echo $row['prayersText'];
+						echo "</a>";
+						echo "</td>";
+						echo "<td>".date('M j, Y', strtotime($row['prayersEntryDate']))."</td>";
+						echo "<td>".$row['prayersReadCount']."</td>";
+						echo "</tr>";
+					} 
+					echo "</table>"; 
 				} 
-				echo "</table>"; 
+				else { 
+					echo "No matching records are found."; 
+				} 
 			} 
 			else { 
-				echo "No matching records are found."; 
+				echo "ERROR: Could not able to execute $sql. " . mysqli_error($link); 
 			} 
-		} 
-		else { 
-			echo "ERROR: Could not able to execute $sql. "
-										.mysqli_error($link); 
-		} 
-		 
 		?>
-
-	
-	
+	</main>
 	<footer>
 		<p>&copy; 2023 Prayers Website. All rights reserved.</p>
 	</footer>
-	</body>
+</body>
 </html>
--- store.php
+++ store.php
@@ -1,37 +1,173 @@
-<?php
-        $messageText = isset($_POST['messageText']) ? $_POST['messageText'] :"";
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Delicious+Handrawn&display=swap" rel="stylesheet">
+  <title>Prayer Website</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f5f5f5;
+      margin: 0;
+			padding: 0;
+    }
 
-        if (empty($messageText)) {
-          echo 'Error: Please fill in all the required fields.';
-        } else {
-            $isql ='INSERT INTO prayers (
-                prayerText
-                ,prayerEntryDate
-                ,prayerReadCount
-                ) VALUES ("'
-                . $_POST['messageText'] .'","'
-                .date("Ymd").'",0)';
-            }
+    header {
+			font-family: 'Pacifico', cursive;
+			background-color: #10846f;
+			color: #ffffff;
+			padding: 20px;
+			font-size: 20px;
+			display: flex;
+			display: flex;
+  		align-items: center;
+		}
 
-    		$link = mysqli_connect("localhost", "milehigh_prayers", "1234userPRAYERS", "milehigh_prayers"); 
-    		  
-    		if ($link == false) { 
-    			die("ERROR: Could not connect. "
-    						.mysqli_connect_error()); 
-    		} 
-    		
-            if($link->query($isql) === TRUE)
-            {
-            	echo "<br> New prayer created successfully";
-            } else {
-            	echo "<br> Error: ".$isql."<br>".$link->error;
-}    
-            
-            
-            
-            
-            
-          echo 'Thank you for your prayer.';
-        
-    //}
-?>
+		h1 {
+			margin: 0;
+		}
+
+		header a {
+			margin-left: 50px;
+			font-family: 'Delicious Handrawn', cursive;
+			color: #ffffff;
+			font-size: 30px;
+		}
+
+		header a:hover {
+			color:#09344c;
+		}
+
+    main {
+      margin: 20px;
+      font-size: 100px;
+      text-align:center;
+      margin-top:-20px;
+      color:#0f557b;
+      font-family: 'Delicious Handrawn', cursive;
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background-color: #ffffff;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+    }
+
+    input[type="submit"] {
+      background-color: #10846f;
+      color: #ffffff;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      font-size: 24px;
+      cursor: pointer;
+      transition: all 0.2s ease;
+    }
+
+    input[type="submit"]:hover {
+      background-color: #09344c;
+    }
+
+    label {
+      font-size: 24px;
+      margin-bottom: 10px;
+    }
+
+    textarea {
+      font-size: 24px;
+      padding: 10px;
+      border-radius: 5px;
+      border: none;
+      resize: vertical;
+      min-height: 200px;
+      margin-bottom: 20px;
+    }
+
+    .error {
+      color: red;
+      font-size: 18px;
+      margin-bottom: 10px;
+    }
+
+    .success {
+      color: #10846f;
+      font-size: 24px;
+      margin-bottom: 10px;
+    }
+
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-top: 40px;
+    }
+
+    th, td {
+      border: 1px solid #cccccc;
+      padding: 10px;
+      text-align: left;
+    }
+
+    th {
+      background-color: #10846f;
+      color: #ffffff;
+    }
+
+    tr:nth-child(even) {
+      background-color: #f5f5f5;
+    }
+  </style>
+</head>
+
+<body>
+  <header>
+    <h1>Prayer Website</h1>
+		<a href="index.html">Home</a>
+		<a href="write.html">Write Prayer</a>
+    <a href="read.php">Read Prayer</a>
+  </header>
+</body>
+
+<main>
+  <?php
+          $messageText = isset($_POST['messageText']) ? $_POST['messageText'] :"";
+
+          if (empty($messageText)) {
+            echo 'Error: Please fill in all the required fields.';
+          } else {
+              $isql ='INSERT INTO prayers (
+                  prayersText
+                  ,prayersEntryDate
+                  ,prayersReadCount
+                  ) VALUES ("'
+                  . $_POST['messageText'] .'","'
+                  .date("Ymd").'",1)';
+              
+
+              $link = mysqli_connect("localhost", "milehigh_prayers", "1234userPRAYERS", "milehigh_prayers"); 
+                
+              if ($link == false) { 
+                die("ERROR: Could not connect. "
+                      .mysqli_connect_error()); 
+              } 
+          
+              if($link->query($isql) === TRUE)
+              {
+                echo "<br> New prayer created successfully. ";
+                echo '<br> Thank you for your prayer!!';
+              } else {
+                echo "<br> Error: ".$isql."<br>".$link->error;
+              }
+          }    
+
+  ?>
+</main>
+
+</html>
